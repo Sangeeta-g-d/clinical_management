96a6ba90e30300ca74454a755b957df32ceafebc
sangeeta-g-d <sangeetadani599@gmail.com>
2024-06-21 15:34:58 +0530

    asd

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('',views.index,name="index"),
+    path('admin_login',views.admin_login,name="admin_login"),
+    path('admin_db',views.admin_db,name="admin_db"),
+    path('approve_clinic/<int:id>',views.approve_clinic,name="approve_clinic"),
+    path('delete_clinic/<int:id>',views.delete_clinic,name="delete_clinic"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from . models import NewUser
+from django.db.models.functions import Trim
+from django.db.models import Q
+from datetime import datetime
 
 # Create your views here.
+def admin_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username, password = password)
+        if user is not None and user.is_superuser == True:
+            login(request,user)
+            return redirect('/admin_db')
+        else:
+            error_message = "Invalid username or password"
+            return render(request,'admin_login.html',{'error_message':error_message})
+    return render(request,'admin_login.html')
+
+def admin_db(request):
+    search_query = request.GET.get('search', '')
+    data = NewUser.objects.filter(user_type='clinic')
+    if search_query:
+        data = data.filter(
+            Q(first_name__icontains=search_query) 
+        )
+    context = {
+        'data':data
+    }
+    return render(request,'admin_db.html',context)
+
+def delete_clinic(request, id):
+    obj = get_object_or_404(NewUser, pk=student_id)
+    obj.delete()
+    return redirect('admin_db')
+
+def approve_clinic(request, id):
+    obj = get_object_or_404(NewUser, pk=student_id)
+    obj.is_staff = True
+    onj.save()
+    return redirect('admin_db')
