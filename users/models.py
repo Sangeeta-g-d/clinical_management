d9a3fdafc6e1004e5e25719e3ae4765047badbbe
vidyawandakar <vidyawandakar@gmail.com>
2024-06-22 12:29:44 +0530

    dfgbdfg

--- users/models.py
+++ users/models.py
@@ -13,9 +13,9 @@ class NewUser(AbstractUser):
     available_timings=models.CharField(max_length=100, default=' ')
     added_by=models.CharField(max_length=100, default=' ')
 
+
 class Appointments(models.Model):
     clinic_id = models.CharField(max_length=100, default='patient')
     patient_id = models.ForeignKey('NewUser', on_delete=models.CASCADE, related_name='patient_appointments')
     doctor_id = models.ForeignKey('NewUser', on_delete=models.CASCADE, related_name='doctor_appointments')
     timings = models.DateTimeField(default=timezone.now)
-    
