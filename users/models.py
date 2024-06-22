131e6d731161a7143e6da5e976c9a6c64e68a4a4
vidyawandakar <vidyawandakar@gmail.com>
2024-06-22 12:26:32 +0530

    dfgbhnj

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import datetime 
+from django.utils import timezone
 
 class NewUser(AbstractUser):
     user_type = models.CharField(max_length=100, default='patient')
@@ -11,3 +12,10 @@ class NewUser(AbstractUser):
     specialization=models.CharField(max_length=100, default=' ')
     available_timings=models.CharField(max_length=100, default=' ')
     added_by=models.CharField(max_length=100, default=' ')
+
+class Appointments(models.Model):
+    clinic_id = models.CharField(max_length=100, default='patient')
+    patient_id = models.ForeignKey('NewUser', on_delete=models.CASCADE, related_name='patient_appointments')
+    doctor_id = models.ForeignKey('NewUser', on_delete=models.CASCADE, related_name='doctor_appointments')
+    timings = models.DateTimeField(default=timezone.now)
+    
